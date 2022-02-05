72983bbea59f48b439531a5569f925bf0fd04f41
Tom Sherman <the.tomsherman@gmail.com>
2022-02-05 18:23:25 +0000

    Refactor prop type

--- src/Link.tsx
+++ src/Link.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import {
 	useNavigate,
 	DefaultGenerics,
-	LinkProps,
+	LinkProps as ReactLocationLinkProps,
 } from "@tanstack/react-location";
 import { Button } from "react-native";
 
+export interface LinkProps<TGenerics>
+	extends ReactLocationLinkProps<TGenerics> {
+	title: string;
+}
+
 export function Link<
 	TGenerics extends Partial<DefaultGenerics> = DefaultGenerics
->(props: LinkProps<TGenerics> & { title: string }) {
+>(props: LinkProps<TGenerics>) {
 	const navigate = useNavigate<TGenerics>();
 
 	return <Button title={props.title} onPress={() => navigate(props)} />;
